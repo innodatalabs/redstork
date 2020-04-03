b90a017724f402c385149ea6e28e9776b2c503ee
Mike Kroutikov <mkroutikov@innodata.com>
2020-04-03 14:50:54 -0400

    fixed setup - more project info

--- setup.py
+++ setup.py
@@ -9,25 +9,30 @@ class bdist_wheel(_bdist_wheel):
         _bdist_wheel.finalize_options(self)
         self.root_is_pure = False
 
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
-    name    = 'redstork',
-    version = __version__,
-    description= 'Parsing PDF files with PDFium',
-    long_description='TODO',
-
-    license='MIT',
+    name        = 'redstork',
+    version     = __version__,
+    description = 'Parsing PDF files with PDFium',
+    author      ='Mike Kroutikov',
+    author_email='mkroutikov@innodata.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url         ='https://github.com/innodatalabs/redstork',
+    license     ='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     packages=find_packages(),
-    package_data = {'': ['linux/*.so', 'win/*.dll', 'pdfium_version.txt']},
+    package_data = {'': ['linux/*.so', 'win/*.dll', 'darwin/*.so', 'pdfium_version.txt']},
     # cmdclass = {'bdist_wheel': bdist_wheel}
+    python_requires='>=3.6',
 )
