b705e33c4e13c88af0b5376c069843ea2a64e8c5
Mike Kroutikov <mkroutikov@innodata.com>
2024-09-10 11:11:31 -0400

    .

--- steps/00-env.sh
+++ steps/00-env.sh
@@ -5,6 +5,6 @@ export PDFium_SOURCE_DIR=$ROOT/pdfium
 export PDFium_BUILD_DIR=$ROOT/pdfium/out
 
 export OS=${PDFium_TARGET_OS:?}
-CPU = ${PDFium_TARGET_CPU:?}
+CPU=${PDFium_TARGET_CPU:?}
 export DEPOT_TOOLS_WIN_TOOLCHAIN=0
 
